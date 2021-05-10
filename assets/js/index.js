397f366f336a3ea0e8a1dee790195906746f61b5
Rafaella.lopes <rafinhacrisdenlavor@gmail.com>
2021-05-10 20:36:23 -0300

    add: pasta e arquivo js

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const descricao = document.querySelector(".apresentacao__descricao");
+
+
+function typeWrite(elemento) {
+    const textoArray = elemento.innerHTML.split("");
+    console.log(textoArray);
+}
+
